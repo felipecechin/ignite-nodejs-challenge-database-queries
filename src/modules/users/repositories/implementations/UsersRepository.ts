4d4dd9f2ba6a80a9300c1059937aba35884383bc
Felipe Cechin <ficechin@hotmail.com>
2022-08-15 15:39:36 -0300

    repositories code complete

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,25 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({
+      id: user_id,
+    }, {
+      relations: ['games']
+    }
+    )
+    if (!user) { throw new Error("User not found") }
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT * FROM "users" order by "first_name"');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`SELECT * FROM users AS usuario WHERE usuario.first_name ILIKE '${first_name}' AND usuario.last_name ILIKE '${last_name}'`);
   }
 }
